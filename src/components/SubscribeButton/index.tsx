2bb12be8b08a2bfc8376382495e5fa23b494d33f
axsgustavo <guhtm100@gmail.com>
2022-09-06 12:35:57 -0300

    Configurando banco de dados FaunaDB

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,16 @@
+import styled from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  return (
+    <button
+      type="button"
+      className={styled.subscribeButton}
+    >
+      Subscribe now
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from "next/head";
+
+import { GetStaticProps } from "next";
+import { stripe } from "../services/stripe";
+
+import { SubscribeButton } from "../components/SubscribeButton";
+
 import styled from "../styles/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -13,10 +26,12 @@ export default function Home() {
           <h1>News abount <br />the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/images/avatar.svg"
           alt="Girl coding"
@@ -24,4 +39,23 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1LYYGRI3BY9j2nQRF7xnTxyw");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
 }
